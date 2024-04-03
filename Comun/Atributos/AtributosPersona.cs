2cfc0d1b6f36ed8bbe23e175aa1c98169943532d
Diorkys <diorkys0204@gmail.com>
2024-04-03 15:13:42 -0400

    Agregando carpeta Comun

---
+++ Comun/Atributos/AtributosPersona.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comun.Atributos
+{
+    public class AtributosPersona
+    {
+        private int Id;
+        private string nombre;
+        private string apellido;
+        private string direccion;
+        private DateTime fecha_nacimiento;
+        private string celular;
+
+        public int Id1 { get => Id; set => Id = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
+        public DateTime Fecha_nacimiento { get => fecha_nacimiento; set => fecha_nacimiento = value; }
+        public string Celular { get => celular; set => celular = value; }
+    }
+}
---
+++ Dominio/Crud/CPersonas.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using AccesoDatos.Entidades;
+using Comun.Atributos;
+
+namespace Dominio.Crud
+{
+    public class CPersonas
+    {
+        Persona persona = new Persona();
+
+        public DataTable Mostrar()
+        { 
+            DataTable td = new DataTable();
+            td = persona.Mostrar();
+            return td;
+        }
+
+        public DataTable Buscar(string Buscar)
+        {
+            DataTable td = new DataTable();
+            td = persona.Buscar(Buscar );
+            return td;
+        }
+
+        public void Insertar(AtributosPersona obj)
+        {
+            persona.Insertar(obj);
+        }
+
+
+
+        public void Modificar(AtributosPersona obj)
+        {
+            persona.Modificar(obj);
+        }
+
+        public void Eliminar(AtributosPersona obj)
+        {
+            persona.Eliminar(obj);
+        }
+    }
+}
